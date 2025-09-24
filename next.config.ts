264a99f83cbec9286bcf91a05cb5b3456c8951ab
nari <yasunarj@gmail.com>
2025-09-24 19:31:00 +0900

    fixed: video_fps

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,24 @@
 // next.config.mjs (または next.config.ts)
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
 
 // request.ts の場所を **明示**（ミスを防ぐ）
-const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
+const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig /**: NextConfig*/ = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/movies/:file*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig);
-
